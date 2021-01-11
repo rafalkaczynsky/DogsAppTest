2c396b82cdb951a6146218b579521bf5d272244a
rkaczynski <rafal.kaczynski@sabstt.com>
2021-01-11 14:14:30 +0000

    Icon instead lable on settings

--- src/components/SettingsButton.tsx
+++ src/components/SettingsButton.tsx
@@ -1,6 +1,8 @@
 import {ReactElement} from 'react';
 import React from 'react';
-import {BaseText, Button} from './Core';
+import Icon from 'react-native-vector-icons/Ionicons'
+import {Button} from './Core';
+import Palette from '../styles/palette';
 
 export interface ButtonProps {
   navigation: any
@@ -9,7 +11,7 @@ const SettingsButton = (props: ButtonProps): ReactElement => {
   const {navigation} = props;
   return (
     <Button onPress={() => navigation.navigate('SettingsScreen')}>
-      <BaseText>Settings</BaseText>
+        <Icon name="settings-outline" size={30} color={Palette.lightest} />
     </Button>
   );
 };
